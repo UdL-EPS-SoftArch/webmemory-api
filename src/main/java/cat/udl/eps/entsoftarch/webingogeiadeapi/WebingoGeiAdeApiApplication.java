fdb8bf888b9b813e516abcf6e60aeb7b45bb35cd
Roberto García <roberto@rhizomik.net>
2019-02-12 21:59:11 +0100

    Initial commit Spring skeleton and samples

---
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiadeapi/WebingoGeiAdeApiApplication.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.entsoftarch.webingogeiadeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebingoGeiAdeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebingoGeiAdeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/WebingoGeiAdeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package cat.udl.eps.entsoftarch.webingogeiadeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebingoGeiAdeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
